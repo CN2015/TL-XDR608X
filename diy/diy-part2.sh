dd210b789144e927991b9d906cf78e845ea73203
CN2015 <164381491+CN2015@users.noreply.github.com>
2025-04-18 22:42:06 +0800

    Update diy-part2.sh

--- diy/diy-part2.sh
+++ diy/diy-part2.sh
@@ -10,19 +10,19 @@
 # See /LICENSE for more information.
 #
 
-# Modify default IP
+# 修改默认IP地址
 #sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# 修改默认主题
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-#修改默认时区
+# 修改默认时区
 sed -i "s/timezone='.*'/timezone='CST-8'/g" ./package/base-files/files/bin/config_generate
 sed -i "/timezone='.*'/a\\\t\t\set system.@system[-1].zonename='Asia/Shanghai'" ./package/base-files/files/bin/config_generate
 
-# Modify hostname
+# 修改系统名称
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
-#补充汉化
+# 补充汉化
 echo -e "\nmsgid \"NAS\"" >> feeds/luci/modules/luci-base/po/zh_Hans/base.po
 echo -e "msgstr \"存储\"" >> feeds/luci/modules/luci-base/po/zh_Hans/base.po
 # 更改菜单名字
